59dd863f4e5076304bdd511d566429dfc0908440
haibolian <957377783@qq.com>
2021-04-27 20:33:41 +0800

    vue2迁移至vue3完毕

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { createApp } from 'vue'
 import router from './router'
 import store from './store'
+
+// element-plus
 import ElementUI from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
+
+// LUI
 import LUI from './LUI/index'
 import './LUI/style/index.scss'
 
+// 注册全局组件
 import globalComponent from './components/index.js'
 
-import App from './App.vue'
-
-import './icons'
+// 样式文件
 import './style/index.css'
+// 引入图标
+import './icons'
 
+import App from './App.vue'
 const app = createApp(App)
 
 app.use(globalComponent)
