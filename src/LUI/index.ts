c287dabf9236499d85d7ded926d8f5413703766d
haibolian <957377783@qq.com>
2021-07-11 22:08:11 +0800

    require.context自动化

--- src/LUI/index.ts
+++ src/LUI/index.ts
@@ -1,31 +1,8 @@
-import LButton from './package/button/index.vue'
-import LMenu from './package/menu/menu.vue'
-import LSubMenu from "./package/menu/submenu.vue"
-import LMenuItem from "./package/menu/menu-item.vue"
-import LInput from "./package/input/index.vue"
-import LRadio from './package/radio/radio.vue'
-import LRadioGroup from './package/radio/radio-group.vue'
-import LCheckbox from './package/checkbox/checkbox.vue'
-import LCheckboxGroup from './package/checkbox/checkbox-group.vue'
-import LSelect from "./package/select/select.vue"
-import LOption from "./package/select/option.vue"
-import LTabs from "./package/tabs/tabs.vue"
-import LTabsPane from './package/tabs/tabs-pane.vue'
-let components = [
-  LButton,
-  LMenu,
-  LSubMenu,
-  LMenuItem,
-  LInput,
-  LRadio,
-  LRadioGroup,
-  LCheckbox,
-  LCheckboxGroup,
-  LSelect,
-  LOption,
-  LTabs,
-  LTabsPane
-]
+let components: any[] = []
+let files = require.context('./package',true,/\.vue$/)
+files.keys().forEach(key=>{
+  components.push(files(key).default)
+})
 
 const install = (Vue:any)=>{
   components.forEach(component=>{
