c5c066d17ddee55b6041ffb1ae6bab7ece172b35
haibolian <957377783@qq.com>
2022-01-18 00:06:46 +0800

    progress

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,64 +1,31 @@
 <template>
-  <div class="about">
-    <l-counter :min="33" :step='3' @change="change" v-model="num"></l-counter>
-    <!-- <el-input-number :min="33" :step='3' @change="change" v-model="num" /> -->
-    <!-- <l-input v-model="num" @blur="handleBlur"></l-input> -->
-    {{num}}
-    <!-- <el-input v-model="num" placeholder="">
-      <template v-slot:prepend>
-        asdasd
-      </template>
-    </el-input> -->
-      <!-- <el-input-number
-    v-model="num"
-    @change="change"
-    :max="10"
-    :min="11"
-    label="描述文字"
-  ></el-input-number> -->
-    <!-- <l-form :model='form' ref="form">
-      <l-form-item prop='username' label='账号'>
-        <l-input v-model="form.username"></l-input>
-      </l-form-item>
-      <l-form-item prop='password' label='密码'>
-        <l-input v-model="form.password"></l-input>
-      </l-form-item>
-    </l-form> -->
+  <div style="width='300px'">
+    <l-progress :percentage='percentage'></l-progress>
+    <l-progress status="success" :percentage='percentage'></l-progress>
+    <l-progress status="warning" :percentage='percentage'></l-progress>
+    <l-progress status="danger" :percentage='percentage'></l-progress>
   </div>
 </template>
 
 <script>
-import { load as getSentence} from 'jinrishici'
-// const jinrishici = require('jinrishici');
 export default {
   name:'About',
+  components:{},
+  props:{},
   data(){
     return{
-      form:{
-        username: '',
-        password: ''
-      },
-      num: 44,
+      percentage: 10
     }
   },
-  computed:{
-
-  },
+  computed:{},
   methods:{
-    loadSentenc(){
-      getSentence(res=>{
-        
-      })
-    },
-    change(eee){
-      console.log('change 了',eee);
-    },
-    handleBlur(ev){
-      this.num = parseInt(ev.target.value)
-    }
+    
   },
-  mounted(){
-    this.loadSentenc()
-  }
-};
+  created(){},
+  mounted(){}
+}
 </script>
+
+<style lang='scss' scoped>
+
+</style>
