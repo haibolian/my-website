90c8fb47351df43a9bf562ec90f9290b57121557
lianhaibo <957377783@qq.com>
2022-04-22 22:13:17 +0800

    初始化collapse

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,122 +1,70 @@
 <template>
-  <!-- <div class="about">
-    <l-input-number v-if="ssss" :precision="2" v-model="numberValue"></l-input-number>
-    <l-input-number v-if="ssss2" :precision="2" v-model="numberValue"></l-input-number> -->
-    <div class="wwwww">
-      <l-table highlight-current-row border height="300" :data="tableData" virtual-list>
-        <l-table-column prop="name" label="姓名"></l-table-column>
-        <l-table-column align="center" prop="age" label="年龄"></l-table-column>
-        <l-table-column prop="address" label="地址"></l-table-column>
-      </l-table>
-    </div>
-
-    <!-- <div class="wwwww">
-      <l-table highlight-current-row border height="300" :data="tableData" @row-click="clickw" @row-dblclick="cliockrr" @currentRow-change="changeCuure">
-        <l-table-column prop="name" label="姓名"></l-table-column>
-        <l-table-column align="center" prop="age" label="年龄"></l-table-column>
-        <l-table-column prop="address" label="地址"></l-table-column>
-      </l-table>
-    </div>
-    <br>
-    <br> -->
-
-    <!-- <l-button @click="ddddd">增加</l-button>
-    <l-button @click="dele">杀出</l-button>
-    <l-button @click="revisename">改名</l-button>
-    <div class="wwwww">
-      <el-table  height="300" border highlight-current-row :data="tableData" @row-click="clickw" @row-dblclick="cliockrr">
-        <el-table-column prop="name" label="姓名">
-          <template v-slot:default="{row}">
-            {{ row.name }}
-          </template>
-        </el-table-column>
-        <el-table-column align="center" prop="age" label="年龄"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
-      </el-table>
-    </div> -->
-  <!-- </div> -->
+  <div class="demo-collapse">
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item title="Consistency" :name="1">
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Feedback" :name="2">
+        <div>
+          Operation feedback: enable the users to clearly perceive their
+          operations by style updates and interactive effects;
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Efficiency" :name="3">
+        <div>
+          Simplify the process: keep operating process simple and intuitive;
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Controllability" :name="4">
+        <div>
+          Decision making: giving advices about operations is acceptable, but do
+          not make decisions for the users;
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+ <div class="demo-collapse">
+    <l-collapse v-model="activeNames" @change="handleChange">
+      <l-collapse-item title="Consistency" :name="1">
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+      </l-collapse-item>
+      <l-collapse-item title="Feedback" :name="2">
+        <div>
+          Operation feedback: enable the users to clearly perceive their
+          operations by style updates and interactive effects;
+        </div>
+      </l-collapse-item>
+      <l-collapse-item title="Efficiency" :name="3">
+        <div>
+          Simplify the process: keep operating process simple and intuitive;
+        </div>
+      </l-collapse-item>
+      <l-collapse-item title="Controllability" :name="4">
+        <div>
+          Decision making: giving advices about operations is acceptable, but do
+          not make decisions for the users;
+        </div>
+      </l-collapse-item>
+    </l-collapse>
+  </div>
 </template>
 
-<script>
-import { load as getSentence} from 'jinrishici'
-// const jinrishici = require('jinrishici');
-export default {
-  name:'About',
-  data(){
-    return{
-      ssss: true,
-      ssss2: true,
-      numberValue: null,
-      tableData: [
-        { name: '张三', age: 24, address: '北京市西城区监狱' },
-        { name: '李斯', age: 22, address: '秦国咸阳' },
-        { name: '李白', age: 22, address: '大唐长安' },
-      ],
-      form:{
-        username: '',
-        password: '',
-      },
-      num: 44,
-    }
-  },
-  computed:{
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-  },
-  methods:{
-
-    clickBoat(callback, ev){
-      callback()
-    },
-    dele(){
-      this.tableData.splice(0,1)
-    },
-    ddddd(){
-      this.tableData.push(
-        { name: '关羽', age: 54, address: '我也不知道哪里' }
-      )
-    },
-    revisename(){
-      this.tableData[0].name = Date.now()
-    },
-    changeCuure(){
-      console.log('change');
-    },
-    clickw(){
-      console.log('asdasd');
-    },
-    cliockrr(){
-      console.log('123123');
-    },
-    initData(){
-      this.tableData = []
-      for (let index = 1; index <= 5000; index++) {
-        this.tableData.push({
-          name: `杭州${index}号`,
-          age: index,
-          address: `西湖区长盛街${index}号`
-        })
-      }
-    },
-    change(eee){
-      console.log('change 了',eee);
-    },
-    handleBlur(ev){
-      this.num = parseInt(ev.target.value)
-    }
-  },
-  mounted(){
-    this.initData()
-  }
-};
+const activeNames = ref([])
+const handleChange = (val: string[]) => {
+  console.log(val)
+}
 </script>
 <style lang="scss" scoped>
-.dock {
-  width: 800px;
-}
-.wwwww{
-  box-sizing: border-box;
-  padding: 20px;
-  width: 100%;
-  // height: 200px;
+.demo-collapse {
+  width: 500px;
 }
 </style>
